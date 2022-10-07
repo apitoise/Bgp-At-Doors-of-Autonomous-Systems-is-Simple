b38efe18c59644ca14b01850a4ba1828b1c6b44e
apitoise <arthur.pitoiset@outlook.fr>
2022-10-07 18:20:02 +0200

    P2 static vxlan almost done i guess

---
+++ config/utils/ip_config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Set up host's eth0 ip
+
+NUMBER=$(echo $HOSTNAME | tr -dc '0-9')/24
+
+if [ $NUMBER = "1" ]
+then
+	LOCAL_ID=1
+else
+	LOCAL_ID=2
+fi
+
+ip addr add 30.1.1.$LOCAL_ID/24 dev eth0
